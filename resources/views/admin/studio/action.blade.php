09011ed66aac18c9a7b2c0babdce1d120554f480
Mehmood97 <mehmood.ismail@saasfa.com>
2022-03-11 11:46:48 +0500

    removed linked for studio details

--- resources/views/admin/studio/action.blade.php
+++ resources/views/admin/studio/action.blade.php
@@ -1,5 +1,7 @@
-<a href="{{ url('customer/edit/'.base64_encode($record->id)) }}" class="btn btn-info btn-xs edit"
-    style="float: center;"><i class="fa fa-info">
-     </i> </a>
+{{-- <a href="{{ url('customer/edit/'.base64_encode($record->id)) }}" class="btn btn-info btn-xs edit"
+    style="float: center;"><i class="fa fa-info"> --}}
+<a href="#" class="btn btn-info btn-xs edit"
+            style="float: center;"><i class="fa fa-info"></i>
+</a>
 
 
