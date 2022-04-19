e78cfb2fc5f1bf700e22baed532985c843ff31d1
Mehmood97 <mehmood.ismail@saasfa.com>
2022-04-19 13:13:26 +0500

    pull master

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+<tr>
+    <td class="header">
+    {{--    <a href="{{ $url }}">--}}
+        {{--    {{ $slot }}--}}
+            <img src="{!! asset('assets/admin/studio-logo.png') !!}" alt="Web Builder" class="img-responsive" style="margin:0 auto; width:150px;" />
+       {{-- </a>--}}
+    </td>
+</tr>
